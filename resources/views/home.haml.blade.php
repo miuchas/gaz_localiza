76db28c7daee02dfaac983eff727f99c09ac5342
Phelipe Diniz <phelipe@task.com.br>
2018-03-09 14:39:30 -0300

    adicionar css background img

--- resources/views/home.haml.blade.php
+++ resources/views/home.haml.blade.php
@@ -3,16 +3,20 @@
 
 %section#home
   .container
-    .box-perguntas-sombra
-      .box-perguntas
-        .texto-do-quadrado
-          %span.bem-vindo
-            Bem-vindo ao nosso
-          %span.bem-vindo.verde
-            Quiz.
-          %span.alugue.verde
-            ALUGUE
-          %span.alugue
-            ESSA IDEIA.
+    .col-md-6
+      .box-perguntas-sombra
+        .box-perguntas
+          .texto-do-quadrado
+            %span.bem-vindo
+              Bem-vindo ao nosso
+            %span.bem-vindo.verde
+              Quiz.
+            %span.alugue.verde
+              ALUGUE
+            %span.alugue
+              ESSA IDEIA.
+    .col-md-6
+      .box-perguntas-sombra.pequeno
+        .box-perguntas.pequeno
 
 @endsection
